de44ae9b3b432d9728dfffe998096ab8b17875c0
JayNguyen14 <saberon1402@gmail.com>
2025-02-18 19:59:23 -0600

    remove comments

--- server/routes/api/note.js
+++ server/routes/api/note.js
@@ -9,10 +9,4 @@ router.get('/:id', auth.requireLogin, controller.show)
 router.patch('/update/:id', auth.requireLogin, controller.update)
 router.post('/delete/:id', auth.requireLogin, controller.remove)
 
-// router.get('/', controller.index)
-// router.post('/create' ,controller.create)
-// router.get('/:id', controller.show)
-// router.patch('/update/:id', controller.update)
-// router.post('/delete/:id', controller.remove)
-
 module.exports = router
