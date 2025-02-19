5b51fb022057d928a5d6dda92d103c2f6324bb60
cngene <ngenech@gmail.com>
2025-02-19 16:45:45 -0600

    Fixing authentication issue with creating a note

--- server/routes/api/note.js
+++ server/routes/api/note.js
@@ -3,10 +3,10 @@ const router = express.Router()
 const controller = require('../../controllers/noteController')
 const auth = require('../../middleware/auth-service')
 
-router.get('/', auth.requireLogin, controller.index)
-router.post('/create', auth.requireLogin ,controller.create)
-router.get('/:id', auth.requireLogin, controller.show)
-router.patch('/update/:id', auth.requireLogin, controller.update)
-router.post('/delete/:id', auth.requireLogin, controller.remove)
+// router.get('/', auth.requireLogin, controller.index)
+router.post('/create', auth.verifyToken, controller.create)
+// router.get('/:id', auth.requireLogin, controller.show)
+// router.patch('/update/:id', auth.requireLogin, controller.update)
+// router.post('/delete/:id', auth.requireLogin, controller.remove)
 
 module.exports = router
