fbf899a4ff72005992c31544c4029213c40c0ea3
JayNguyen14 <saberon1402@gmail.com>
2025-02-21 15:14:46 -0600

    Fixed some mismatched calls in the implementation of routes/note and routes/folder

--- server/routes/api/folder.js
+++ server/routes/api/folder.js
@@ -4,18 +4,18 @@ const controller = require('../../controllers/folderController')
 const auth = require('../../middleware/auth-service')
 
 // Get all folders
-router.get('/', auth.requireLogin, controller.index)
+router.get('/', auth.verifyToken, controller.index)
 
 // Create a new folder
-router.post('/create', auth.requireLogin ,controller.create)
+router.post('/create', auth.verifyToken ,controller.create)
 
 // Get a specific folder
-router.get('/:id', auth.requireLogin, controller.show)
+router.get('/:id', auth.verifyToken, controller.show)
 
 // Update a specific folder
-router.patch('/:id', auth.requireLogin, controller.update)
+router.patch('/:id', auth.verifyToken, controller.update)
 
 // Delete a specific folder
-router.delete('/:id', auth.requireLogin, controller.remove)
+router.delete('/:id', auth.verifyToken, controller.remove)
 
 module.exports = router
--- server/routes/api/note.js
+++ server/routes/api/note.js
@@ -4,24 +4,24 @@ const controller = require('../../controllers/noteController')
 const auth = require('../../middleware/auth-service')
 
 // Get all notes
-router.get('/', auth.requireLogin, controller.index)
+router.get('/', auth.verifyToken, controller.index)
 
 // Get all notes in folder
-router.get('/:folderID/note', auth.requireLogin, controller.indexFolder)
+router.get('/:folderID/note', auth.verifyToken, controller.indexFolder)
 
 // Get a specific note
-router.get('/:id', auth.requireLogin, controller.show)
+router.get('/:id', auth.verifyToken, controller.show)
 
 // Create a new note in folder
-router.post('/:folderID/create', auth.requireLogin ,controller.createInFolder)
+router.post('/:folderID/create', auth.verifyToken ,controller.createInFolder)
 
 // Create a free note
-router.post('/create', auth.requireLogin ,controller.create)
+router.post('/create', auth.verifyToken ,controller.create)
 
 // Update a specific note
-router.patch('/:id', auth.requireLogin, controller.update)
+router.patch('/:id', auth.verifyToken, controller.update)
 
 // Delete a specific note
-router.delete('/:id', auth.requireLogin, controller.remove)
+router.delete('/:id', auth.verifyToken, controller.remove)
 
 module.exports = router
