7220781da184c10afcf99720c0be2e082bc006d0
cngene <ngenech@gmail.com>
2025-02-20 21:11:49 -0600

    Updating note methods to fix issues

--- server/routes/api/note.js
+++ server/routes/api/note.js
@@ -3,10 +3,10 @@ const router = express.Router()
 const controller = require('../../controllers/noteController')
 const auth = require('../../middleware/auth-service')
 
-// router.get('/', auth.requireLogin, controller.index)
+router.get('/', auth.verifyToken, controller.index)
 router.post('/create', auth.verifyToken, controller.create)
-// router.get('/:id', auth.requireLogin, controller.show)
-// router.patch('/update/:id', auth.requireLogin, controller.update)
-// router.post('/delete/:id', auth.requireLogin, controller.remove)
+router.get('/:id', auth.verifyToken, controller.show)
+router.patch('/update/:id', auth.verifyToken, controller.update)
+router.post('/delete/:id', auth.verifyToken, controller.remove)
 
 module.exports = router
