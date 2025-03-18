5cfae04f56c81238c3e9045796bb63a7ed539494
cngene <ngenech@gmail.com>
2025-03-17 22:09:42 -0500

    Flashcard page setup

--- client/src/views/features/FlashcardView.vue
+++ client/src/views/features/FlashcardView.vue
@@ -1,15 +1,75 @@
 <template>
-    <div>
-        
+  <div
+    class="container-fluid text-start d-flex flex-column navbar-offset vh-navbar-offset"
+  >
+    <!-- Edit and Self-quiz buttons -->
+    <div
+      class="flashcard-buttons d-flex align-items-center justify-content-start mt-3 ms-1"
+    >
+      <button
+        class="btn btn-secondary btn-circle me-2"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#side_bar_left"
+        v-on:click="flashcardEditMode = false"
+      >
+        Edit
+      </button>
+      <button
+        class="btn btn-secondary btn-circle"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#side_bar_left"
+        v-on:click="quizMode = false"
+      >
+        Self-quiz
+      </button>
     </div>
+
+    <!-- Flashcard Main Screen -->
+    <div class="row mt-3 gx-3">
+        <!-- Flashcards Box -->
+      <div class="col-md-6">
+        <div class="flashcard-list-box">
+          <p>List of Flashcards here</p>
+        </div>
+      </div>
+
+      <div class="col-md-6">
+        <!-- Self Quiz Box -->
+        <div class="self-quiz-box mb-3">
+          <p>Sample question here</p>
+        </div>
+
+        <!-- Questions Box -->
+        <div class="question-list-box">
+          <p>List of Questions here</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-
-<script setup>
-
-</script>
-
+<script setup lang="js"></script>
 
 <style>
+.flashcard-list-box {
+  height: 775px;
+  background-color: #f8f9fa;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  padding: 20px;
+  text-align: center;
+  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
+}
 
+.self-quiz-box, .question-list-box {
+  height: 377px;
+  background-color: #f8f9fa;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  padding: 20px;
+  text-align: center;
+  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
+}
 </style>
