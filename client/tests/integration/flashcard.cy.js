e35408e4dfc7eff76097038b279960703b910f2f
Esbanana <77253631+Esbanana@users.noreply.github.com>
2025-03-21 01:15:29 -0500

    update flashcard.cy.js

--- client/tests/integration/flashcard.cy.js
+++ client/tests/integration/flashcard.cy.js
@@ -2,8 +2,11 @@ describe("Integration Tests - Frontend + Backend", () => {
   let testEmail = "1@email.com";
   let testPassword = "1";
 
+  
+
+  // User Login Test via UI - successful
   it("should log in through the UI and redirect to homepage", () => {
-    cy.visit("/#/login");
+    cy.visit("http://localhost:8080/#/login");
 
     cy.get("#email").type(testEmail);
     cy.get("#password").type(testPassword);
@@ -13,6 +16,19 @@ describe("Integration Tests - Frontend + Backend", () => {
     cy.url().should("include", "/");
   });
   it("we should be able to access the flashcards from the homepage (from features section)", () => {
-      
+    cy.visit("http://localhost:8080/#/");
+    cy.wait(2500);
+    
+    cy.contains('.card','Flash Cards').click()
+    cy.wait(2500);
+    cy.url().should("include", "/flashcards");
+    //cy.get('.card-body').should('have.attr', 'href').and('include', '#/flashcards');
   });
+  it("we should be able to access the flashcards from the homepage (from widgetbar)", () => {
+    cy.visit("http://localhost:8080/#/");
+    cy.wait(2500);
+    cy.contains('.nav-item','Flash Cards').click()
+   
+  });
+ 
 });
